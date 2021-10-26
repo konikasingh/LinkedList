b492826f753a21719f4dcb6e5fbb56b85f3c54cb
konikasingh <konikasingh1996@gmail.com>
2021-10-26 20:33:42 +0530

    InsertLast Method of 56 30 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -6,7 +6,11 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList Programs");
+            CustomLinkedList list = new CustomLinkedList();
+            list.InsertLast(56);
+            list.InsertLast(30);
+            list.InsertLast(70);
+            list.display();
         }
     }
 }
