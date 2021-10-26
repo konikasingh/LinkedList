8c4d5c544c5b663bf7f73fd45f70d10ed99a55a9
konikasingh <konikasingh1996@gmail.com>
2021-10-26 20:46:03 +0530

    Sequence LinkedList using InsertFirst Method

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,9 +7,15 @@ namespace LinkedList
         static void Main(string[] args)
         {
             CustomLinkedList list = new CustomLinkedList();
-            list.InsertLast(56);
-            list.InsertLast(30);
-            list.InsertLast(70);
+            //list.InsertLast(56);
+            //list.InsertLast(30);
+            //list.InsertLast(70);
+            //list.display();
+
+            list.InsertFirst(70);
+            list.InsertFirst(30);
+            list.InsertFirst(56);
+            Console.WriteLine("after inserting node the list is:");
             list.display();
         }
     }
